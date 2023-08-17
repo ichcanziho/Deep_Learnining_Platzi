102f5c362744612edd5fc609ca50bada0f17e47b
Gabriel Ichcanziho <gperez@k2i.mx>
2023-08-17 11:49:25 -0600

    Curso 13: LangChain C21

---
+++ 13 Curso de LangChain/scripts/12_openai_embeddings.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+load_dotenv("../secret/keys.env")
+os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+
+
+from langchain.embeddings import OpenAIEmbeddings
+
+embedding_openai = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+print(embedding_openai)
+
+documentos_a_incrustar = [
+    "¡Hola parce!",
+    "¡Uy, hola!",
+    "¿Cómo te llamas?",
+    "Mis parceros me dicen Omar",
+    "¡Hola Mundo!"
+  ]
+
+incrustaciones = embedding_openai.embed_documents(documentos_a_incrustar)
+
+print(len(incrustaciones[3]))
+
+consulta_incrustada = embedding_openai.embed_query(documentos_a_incrustar[0])
+
+print(consulta_incrustada)
